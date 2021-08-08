4d956a935c6968e3e04cac2d03efac4db92fdd94
Joao Carneiro <jvmcarneiro@gmail.com>
2021-08-07 21:25:20 -0300

    Adiciona script inicial

---
+++ home/Experimentos/Arquivos/Testes Interface Virtual Quartus/virtual_input_test/output_files/virtual_input_test.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE115F29) Path("/home/Experimentos/Arquivos/tests/virtual_input_test/output_files/") File("virtual_input_test.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- largrad-setup.sh
+++ largrad-setup.sh
@@ -1,5 +1,50 @@
 #/bin/bash
 
+# Setup users
+sudo apt install xfce4 xfce4-goodies
+sudo apt install rsync
+sudo apt remove xscreensaver
+
+sudo useradd -m aluno-grad
+sudo passwd aluno-grad
+sudo useradd -m Experimentos
+sudo passwd Experimentos
+sudo usermod -a -G aluno-grad $USER
+sudo usermod -a -G Experimentos $USER
+
+sudo rsync -a home /home
+sudo rsync -a etc /etc
+sudo rsync -a usr /usr
+
+sudo chattr -R +i /home/aluno-grad/.config/autostart/
+sudo chattr -R +i /home/aluno-grad/.config/menus/
+sudo chattr -R +i /home/aluno-grad/.config/xfce4/
+sudo chattr -R +i /home/aluno-grad/.local/bin/
+sudo chattr -R +i /home/aluno-grad/.local/share/applications/
+sudo chattr +i /home/aluno-grad/.vnc/config
+sudo chattr +i /home/aluno-grad/Arquivos/virtual_input.v
+sudo chattr +i /home/aluno-grad/Arquivos/virtual_input_pins.csv
+sudo chattr +i /home/aluno-grad/.bash_login
+sudo chattr +i /home/aluno-grad/.bash_logout
+sudo chattr +i /home/aluno-grad/.bash_profile
+sudo chattr +i /home/aluno-grad/.bashrc
+sudo chattr +i /home/aluno-grad/.inputrc
+sudo chattr +i /home/aluno-grad/.profile
+
+sudo chattr -R +i /home/Experimentos/.config/menus/
+sudo chattr -R +i /home/Experimentos/.config/xfce4/
+sudo chattr -R +i /home/Experimentos/.local/bin/
+sudo chattr -R +i /home/Experimentos/.local/share/applications/
+sudo chattr +i /home/Experimentos/.vnc/config
+sudo chattr +i /home/Experimentos/Arquivos/virtual_input.v
+sudo chattr +i /home/Experimentos/Arquivos/virtual_input_pins.csv
+sudo chattr +i /home/Experimentos/.bash_login
+sudo chattr +i /home/Experimentos/.bash_logout
+sudo chattr +i /home/Experimentos/.bash_profile
+sudo chattr +i /home/Experimentos/.bashrc
+sudo chattr +i /home/Experimentos/.inputrc
+sudo chattr +i /home/Experimentos/.profile
+
 
 
 # Install TigerVNC
@@ -13,28 +58,23 @@ sudo chown -R root:root usr
 sudo tar czf usr.tgz usr
 sudo tar xzkf usr.tgz -C /
 
+mkdir -p /etc/lightdm
+touch /etc/lightdm/lightdm.conf
+echo "[XDMCPServer]" | sudo tee -a /etc/lightdm/lightdm.conf > /dev/null
+echo "enabled=true" | sudo tee -a /etc/lightdm/lightdm.conf > /dev/null
+echo "port=177" | sudo tee -a /etc/lightdm/lightdm.conf > /dev/null
 
+mkdir -p /etc/gdm
+touch /etc/gdm/custom.conf
+echo "[xmdcp]" | sudo tee -a /etc/gdm/custom.conf > /dev/null
+echo "Enabled=true" | sudo tee -a /etc/gdm/custom.conf > /dev/null
+echo "Port=177" | sudo tee -a /etc/gdm/custom.conf > /dev/null
 
-# Setup users
-sudo apt install xfce4 xfce4-goodies
+sudo systemctl restart lightdm
+sudo systemctl restart gdm
 
-sudo useradd -m aluno-grad
-sudo passwd aluno-grad
-sudo useradd -m Experimentos
-sudo passwd Experimentos
+sudo systemctl start xvnc.socket
+sudo systemctl enable xvnc.socket
 
-# COPY FILES
-
-sudo chattr -R +i /home/aluno-grad/.config/autostart/
-sudo chattr -R +i /home/aluno-grad/.config/menus/
-sudo chattr -R +i /home/aluno-grad/.local/bin/
-sudo chattr -R +i /home/aluno-grad/.local/share/applications/
-sudo chattr +i /home/aluno-grad/.vnc/config
-sudo chattr +i /home/aluno-grad/Arquivos/virtual_input.v
-sudo chattr +i /home/aluno-grad/Arquivos/virtual_input_pins.csv
-sudo chattr +i /home/aluno-grad/.bash_login
-sudo chattr +i /home/aluno-grad/.bash_logout
-sudo chattr +i /home/aluno-grad/.bash_profile
-sudo chattr +i /home/aluno-grad/.bashrc
-sudo chattr +i /home/aluno-grad/.inputrc
-sudo chattr +i /home/aluno-grad/.profile
+sudo -u Experimentos vncpasswd -f <<<"larexp2021" >"/home/Experimentos/.vnc/passwd"
+sudo chattr +i /home/Experimentos/.vnc/config
